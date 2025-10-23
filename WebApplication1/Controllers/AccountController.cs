d07f092b2b75a9bd68bdc5a3be3aa4ad0a28f855
Priscilla Kalevor <kalevorpriscilla03@gmail.com>
2025-10-23 02:49:55 +0000

    Initial commit

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Example: hardcoded credentials
+                if (model.Username == "student" && model.Password == "password")
+                {
+                    // You can set session, cookie, or use ASP.NET Identity
+                    TempData["Message"] = "Login successful!";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password");
+                }
+            }
+            return View(model);
+        }
+    }
+}
